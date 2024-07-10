76a84967e3aab87b38185c5782a2406e10c0c5e4
Ryan Weidling <ryanweidling@gmail.com>
2024-07-10 16:28:52 -0400

    added register function to register page

--- client/README.md
+++ client/README.md
@@ -1,3 +1,10 @@
+# Project Purpose
+Learning
+* MongoDB
+* Express
+* React
+* Node
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- client/src/Pages/RegisterPage.js
+++ client/src/Pages/RegisterPage.js
@@ -1,9 +1,23 @@
+import { useState } from "react";
+
 export default function RegisterPage() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    function register(ev) {
+        ev.preventDefault();
+        fetch('http:localhost/4000', {
+            method: 'POST', 
+            body: JSON.stringify({username,password}), 
+            headers: {'Content-Type':'application/json'},
+        })
+    }
+
     return(
         <form className="register">
             <h1>Register</h1>
-            <input type="text" placeholder="username"></input>
-            <input type="password" placeholder="password"></input>
+            <input type="text" placeholder="username" value={username} onChange={ev => setUsername(ev.target.value)}></input>
+            <input type="password" placeholder="password" value={password} onChange={ev = setPassword(ev.target.value)}></input>
             <button>Register</button>
         </form>
     );
