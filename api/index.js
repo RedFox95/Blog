531548c81f90c8663d928f2df33574d6a1882637
Ryan Weidling <ryanweidling@gmail.com>
2024-07-11 12:16:09 -0400

    added express routing and mongodb connection

--- api/index.js
+++ api/index.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const PORT = 4000;
 
-app.post('/register', (req, res, next) => {
-    res.json('Registration test successful');
+app.use(cors());
+app.use(express.json());
+
+app.post('/register', (req, res) => {
+    const {username, password} = req.body;
+    res.json({requestData:{username,password}});
 });
+
+// app.get('/test', (req, res) => {
+//     res.json('Working');
+// });
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
 });
+
+//2nyhAQ6hhWXBEhLy
+
+//mongodb + srv://ryanweidling:2nyhAQ6hhWXBEhLy@cluster0.acnrgwj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
--- client/src/Pages/RegisterPage.js
+++ client/src/Pages/RegisterPage.js
@@ -4,20 +4,24 @@ export default function RegisterPage() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
-    function register(ev) {
-        ev.preventDefault();
-        fetch('http:localhost/4000', {
-            method: 'POST', 
-            body: JSON.stringify({username,password}), 
-            headers: {'Content-Type':'application/json'},
-        })
+    async function register(e) {
+        e.preventDefault();
+        try {
+            await fetch('http://localhost:4000/register', {
+                method: 'POST',
+                body: JSON.stringify({ username, password }),
+                headers: { 'Content-Type': 'application/json' },
+            });
+        } catch (error) {
+            console.error('Error registering:', error);
+        }
     }
 
     return(
-        <form className="register">
+        <form className="register" onSubmit={register}>
             <h1>Register</h1>
-            <input type="text" placeholder="username" value={username} onChange={ev => setUsername(ev.target.value)}></input>
-            <input type="password" placeholder="password" value={password} onChange={ev = setPassword(ev.target.value)}></input>
+            <input type="text" placeholder="username" value={username} onChange={e => setUsername(e.target.value)}></input>
+            <input type="password" placeholder="password" value={password} onChange={e => setPassword(e.target.value)}></input>
             <button>Register</button>
         </form>
     );
