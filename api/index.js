b8d62ab931cff5beb9b09db6d92561da1806adcf
Ryan Weidling <ryanweidling@gmail.com>
2024-07-11 15:13:11 -0400

    added user schema, login function, imported bcrypt

--- api/index.js
+++ api/index.js
@@ -1,24 +1,38 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { default: mongoose } = require('mongoose');
+const User = require('./models/User');
+const bcrypt = require('bcryptjs');
 const PORT = 4000;
+
+const salt = bcrypt.genSaltSync(10);
 
 app.use(cors());
 app.use(express.json());
 
-app.post('/register', (req, res) => {
+mongoose.connect('mongodb+srv://ryanweidling:2nyhAQ6hhWXBEhLy@cluster0.acnrgwj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+
+app.post('/register', async (req, res) => {
     const {username, password} = req.body;
-    res.json({requestData:{username,password}});
+    try {
+        const userDoc = await User.create({ 
+            username,
+            password: bcrypt.hashSync(password, salt),
+        });
+        res.json(userDoc);
+    } catch (e) {
+        console.log(e);
+        res.status(400).json(e);
+    }
 });
 
-// app.get('/test', (req, res) => {
-//     res.json('Working');
-// });
+app.post('/login', async (req, res) => {
+    const {username, password} = req.body;
+    const userDoc = await User.findOne({username});
+    res.json(userDoc);
+});
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
 });
-
-//2nyhAQ6hhWXBEhLy
-
-//mongodb + srv://ryanweidling:2nyhAQ6hhWXBEhLy@cluster0.acnrgwj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
--- client/src/Pages/LoginPage.js
+++ client/src/Pages/LoginPage.js
@@ -1,9 +1,23 @@
+import { useState } from "react";
+
 export default function LoginPage() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    async function login(e) {
+        e.preventDefault();
+        await fetch('http://localhost:4000/login', {
+            method: 'POST', 
+            body: JSON.stringify({username,password}),
+            headers: {'Content-Type':'application/json'},
+        })
+    }
+
     return (
         <form className="login">
             <h1>Login</h1>
-            <input type="text" placeholder="username"></input>
-            <input type="password" placeholder="password"></input>
+            <input type="text" placeholder="username" onChange={e => setUsername(e.target.value)}></input>
+            <input type="password" placeholder="password" onChange={e => setPassword(e.target.value)}></input>
             <button>Login</button>
         </form>
     );
